3066e93e70e0d726c602d4afaf494a04c0a853d9
MilkyMiku <aaronspong1212@gmail.com>
2020-02-27 23:24:42 +0000

    Updated help command

--- logic/tags/tag-help.js
+++ logic/tags/tag-help.js
@@ -1,9 +1,12 @@
 function help_tag(message) {
     return message.util.sendCode("bash",
-`.t [tag_name] // Shows a tag\n
-.t add [tag_name] [tag_description] // Adds a tag\n
-.t list displays a list of all tags\n
-.t edit [tag_name] [new_tag_description] // Edits a tag`
+`.t [tag_name] // Prints a tag from the database\n
+.t add [tag_name] [tag_description] // Adds a tag to the database\n
+.t list // Prints a list of all tags\n
+.t list [user] // Prints a list of a user's tags\n
+.t edit [tag_name] [new_tag_description] // Edits a tag\n
+.t delete [tag_name] //  If you're the owner, deletes a tag\n 
+.t owner [tag_name] // Prints a tag's owner`
     );
 }
 
